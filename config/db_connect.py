9c4a04e101be693895258fb2af652c789c3e7080
inmonim <cat1083014@gmail.com>
2024-08-21 16:57:44 +0900

    fix : env 설정 수정

--- config/db_connect.py
+++ config/db_connect.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-from .env import DB_HOST, DB_PASSWORD, DB_PORT, DB_USER, DB_TABLE
+from .environment import (CALINIFY_DATABASE_HOST,
+                  CALINIFY_DATABASE_PASSWORD,
+                  CALINIFY_DATABASE_PORT,
+                  CALINIFY_DATABASE_USERNAME,
+                  CALINIFY_DATABASE_TABLE_NAME)
 
-_url = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_TABLE}"
+_url = f"mysql+pymysql://{CALINIFY_DATABASE_USERNAME}:{CALINIFY_DATABASE_PASSWORD}@{CALINIFY_DATABASE_HOST}:{CALINIFY_DATABASE_PORT}/{CALINIFY_DATABASE_TABLE_NAME}"
 
 _engine = create_engine(url=_url, echo=True)
 

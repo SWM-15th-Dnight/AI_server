41d5e88b40fdc9045a72a36a4585c942cdddbfab
inmonim <cat1083014@gmail.com>
2024-08-29 18:37:05 +0900

    feat : loggin 추가 및 db connection error 해결

--- config/db_connect.py
+++ config/db_connect.py
@@ -1,6 +1,8 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import create_engine, text, Engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
+from sqlalchemy.pool import QueuePool
 
+from .logging import logger
 from .environment import (DB_HOST,
                   DB_PASSWORD,
                   DB_PORT,
@@ -9,7 +11,7 @@ from .environment import (DB_HOST,
 
 _url = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_TABLE_NAME}"
 
-_engine = create_engine(url=_url, echo=True)
+_engine = create_engine(url=_url, echo=True, pool_recycle=28700)
 
 _Session = sessionmaker(bind=_engine, autocommit=False, autoflush=True)
 
@@ -22,3 +24,22 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def refresh_connection_pool(engine : Engine = _engine):
+    conn = engine.connect()
+    try:
+        conn.execute(text("SELECT 1"))
+        logger.info("refresh!")
+        
+        pool = engine.pool
+        if isinstance(pool, QueuePool):
+            logger.info(f"Checked-in connections: {pool.checkedin()}")
+            logger.info(f"Checked-out connections: {pool.checkedout()}")
+            logger.info(f"Overflow connections: {pool.overflow()}")
+        
+        conn.close()
+    except:
+        logger.warning(f"DB Connection Error!! Check the DB Connection pool!!")
+        conn.close()
+    finally:
+        conn.close()

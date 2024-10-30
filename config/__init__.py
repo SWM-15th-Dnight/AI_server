34a8e5a13edf7d6edab37fd465d2ae247f02b905
inmonim <cat1083014@gmail.com>
2024-10-30 15:02:02 +0900

    feat: 이미지를 통한 등록 기능 추가

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,6 @@
 from .db_connect import get_db
 from .environment import OPENAI_API_KEY
-from .environment import GPT_PLAIN_TEXT_MODEL
+from .environment import GPT_PLAIN_TEXT_MODEL, GPT_IMAGE_MODEL
 from .db_connect import Base, refresh_connection_pool
 
 from .logging import logger
